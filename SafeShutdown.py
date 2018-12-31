a12e87329813d1a7f99219cdd60f45b15194f412
Lee Witek <neomech@gmail.com>
2018-12-31 19:00:07 +0000

    kills kodi as well as emulationstation, still shuts down if failed to kill either

--- SafeShutdown.py
+++ SafeShutdown.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 from gpiozero import Button, LED
-import os 
+import os
 from signal import pause
 
-powerPin = 3 
-resetPin = 2 
-ledPin = 14 
-powerenPin = 4 
+powerPin = 3
+resetPin = 2
+ledPin = 14
+powerenPin = 4
 hold = 1
 led = LED(ledPin)
 led.on()
 power = LED(powerenPin)
 power.on()
 
-#functions that handle button events
+# functions that handle button events
+
+
 def when_pressed():
-  led.blink(.2,.2)
-  os.system("sudo killall emulationstation && sleep 5s && sudo shutdown -h now")
+    led.blink(.2, .2)
+    os.system("sudo killall emulationstation && sleep 5s")
+    os.system("sudo killall kodi && sleep 5s")
+    os.system("sudo shutdown -h now")
+
+
 def when_released():
-  led.on()
-def reboot(): 
-  os.system("sudo killall emulationstation && sleep 5s && sudo reboot")
-  
+    led.on()
+
+
+def reboot():
+    os.system("sudo killall emulationstation && sleep 5s")
+    os.system("sudo killall kodi && sleep 5s")
+    os.system("sudo reboot")
+
+
 btn = Button(powerPin, hold_time=hold)
 rebootBtn = Button(resetPin)
-rebootBtn.when_pressed = reboot 
+rebootBtn.when_pressed = reboot
 btn.when_pressed = when_pressed
 btn.when_released = when_released
 pause()
